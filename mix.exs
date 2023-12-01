7aea40d8cdbcfbaee00a9f811c430430a0f424a7
Raphael Bellec <raphael.bellec@gmail.com>
2023-12-01 08:37:34 +0100

    Add a lens lib, because why not ? I loved lenses in Haskell and would like to use it more often.

--- mix.exs
+++ mix.exs
@@ -21,6 +21,7 @@ defmodule AdventOfCode2023.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:pathex, "~> 2.0"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
