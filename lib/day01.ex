8168483f570cf0de9219475f14c2f74b1bddb440
Raphael Bellec <raphael.bellec@gmail.com>
2023-12-01 12:16:21 +0100

    First draft of day 1 part 2. Missed overlapping numbers.

--- lib/day01.ex
+++ lib/day01.ex
@@ -1,6 +1,6 @@
 defmodule Day01.Part1 do
-  import Pathex
-  import Pathex.Lenses
+  # import Pathex
+  # import Pathex.Lenses
   @moduledoc """
   Documentation for `Day01.Part1`.
   """
@@ -31,13 +31,20 @@ defmodule Day01.Part2 do
     input
     |> String.split("\n", trim: true)
     |> Enum.map(&line_value/1)
+    # |> Enum.join("\n")
     |> Enum.sum()
   end
 
   def line_value(line) do
-    digits = Regex.scan(~r/(\d|one|two|three|four|five|six|seven|eight|nine)/, line) |> List.flatten()
+    # Trick : some lines have only one digit. Some numbers overlaps, ex: oneight
+    # This is the version without considering this since I would like to test unit testing in elixir for thoses cases
+    # I did not find how to capture all overlapping subexpressions in a string yet, will take just first and last probably using lookahead.
+    # digit_expression = "\d|one|two|three|four|five|six|seven|eight|nine"
+    digits = Regex.scan(~r/\d|one|two|three|four|five|six|seven|eight|nine/, line)|> Enum.map(&(Enum.at(&1, 0)))
 
-    [Enum.at(digits, 0), Enum.at(digits, -1)]
+    [head|tail] = digits
+
+    [head, Enum.at(tail, -1)]
     |> Enum.map(&number_to_digit/1)
     |> Enum.join()
     |> String.to_integer()
@@ -45,6 +52,7 @@ defmodule Day01.Part2 do
 
   def number_to_digit(string) do
     case string do
+      nil -> ""
       "one" -> "1"
       "two" -> "2"
       "three" -> "3"
@@ -66,10 +74,10 @@ defmodule Mix.Tasks.Day01 do
   def run(_) do
     # {:ok, input} = File.read("inputs/demo-input.txt")
     # input = "1abc2\npqr3stu8vwx\na1b2c3d4e5f\ntreb7uchet\n"
-    input = "two1nine\neightwothree\nabcone2threexyz\nxtwone3four\n4nineeightseven2\nzoneight234\n7pqrstsixteen\n"
+    # input = "two1nine\neightwothree\nabcone2threexyz\nxtwone3four\n4nineeightseven2\nzoneight234\n7pqrstsixteen\n"
 
-    # input_filename = "inputs/day01.txt"
-    # {:ok, input} = File.read(input_filename)
+    input_filename = "inputs/day01.txt"
+    {:ok, input} = File.read(input_filename)
 
     # IO.puts("--- Part 1 ---")
     # IO.puts(Day01.Part1.solve(input))
