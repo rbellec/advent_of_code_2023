28c7f12926183e6de6d5d62aa6d46d45bdc9ae02
Raphael Bellec <raphael.bellec@gmail.com>
2023-12-01 08:46:10 +0100

    Add a template for daily problems

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 
 I had a love story with Erlang younger and wanted to get back to it. Of course ~we~ I were young and once I grew up and listened to everyone else telling me that Erlang is not a good language for anything, I tries to forget about it. Then Elixir came and doing an AoC in Elixir is a good way to mix fun with a language and fun with AoC (and maybe some learning).
 
+About libs, did I try to use some tools before I even looked at the first problem ?
+You guessed ! I saw there war a Optics lib in Elixir found it very interesting in Haskell. Not sure I will use deeply nested structures.
+
+
+
---
+++ lib/code_template.ex
@@ -0,0 +1,49 @@
+defmodule Day0.Part1 do
+  @moduledoc """
+  Documentation for `Day01.Part1`.
+  """
+
+  def solve(input) do
+    input
+    |> parse_input()
+    # ...
+  end
+
+  def parse_input(input) do
+    input
+    |> String.split("\n\n")
+    # ...
+  end
+end
+
+defmodule Day0.Part2 do
+  @moduledoc """
+  Documentation for `Day01.Part1`.
+  """
+
+  def solve(input) do
+    input
+    |> Day0.Part1.parse_input()
+    # ...
+  end
+
+end
+
+defmodule Mix.Tasks.Day0 do
+  use Mix.Task
+
+  @spec run(any()) :: :ok
+  def run(_) do
+    demo_input = ""
+
+    input_filename = "inputs/day00.txt"
+    # input_filename = "inputs/demo-input.txt"
+    {:ok, input} = File.read(input_filename)
+
+    IO.puts("--- Part 1 ---")
+    IO.puts(Day0.Part1.solve(input))
+    IO.puts("")
+    IO.puts("--- Part 2 ---")
+    IO.puts(Day0.Part2.solve(input))
+  end
+end
