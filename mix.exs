776fe88e4906ddc5697c829950d20b53d0eb0cab
Raphael Bellec <raphael.bellec@gmail.com>
2023-12-02 15:37:43 +0100

    Add nimble parsec package

--- mix.exs
+++ mix.exs
@@ -21,7 +21,8 @@ defmodule AdventOfCode2023.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:pathex, "~> 2.0"}
+      {:pathex, "~> 2.0"},
+      {:nimble_parsec, "~> 1.0"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
