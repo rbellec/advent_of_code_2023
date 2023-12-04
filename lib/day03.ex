85aa5b0629a80aa748ea6147da89d26d99493919
Raphael Bellec <raphael.bellec@gmail.com>
2023-12-04 13:49:57 +0100

    Day 3 validated ! Squashed commit of the following:
    commit 9c456686d3d6e2fdc5db45031555d0bdff7843c1
    Author: Raphael Bellec <raphael.bellec@gmail.com>
    
        Day 3 validated
    
    commit 47bc696087f2c830e48099505ef5c08cabc1a606
    Author: Raphael Bellec <raphael.bellec@gmail.com>
    
        Results ok with tests data
    
    commit 23d780c280e3e3e7a15e14a3fcdec710c04a86aa
    Author: Raphael Bellec <raphael.bellec@gmail.com>
    
        Unit tests ok for part 2 functions
    
    commit a153d5d58329fa0cf890ff864747b9edfb628fe1
    Author: Raphael Bellec <raphael.bellec@gmail.com>
    
        Get a broader definition of a symbol
    
    commit 2e2b0c8c91040f537ff23cbec94fd984d2ed1603
    Author: Raphael Bellec <raphael.bellec@gmail.com>
    
        Test with redit grid
    
    commit bb8792a036f030480ab0b43e37427568f4c46283
    Author: Raphael Bellec <raphael.bellec@gmail.com>
    
        Works on AoC demo test case.
    
    commit 344ba8ac82734dba8c2ba3921088a63389f20fb3
    Author: Raphael Bellec <raphael.bellec@gmail.com>
    
        Everything ok for part 1.
    
    commit 5b786c0a0ebd193dfe2a6f49a582b5eaf6ba9f87
    Author: Raphael Bellec <raphael.bellec@gmail.com>
    
        Working for different uses cases
    
    commit 61e36528ef414597649dd5613f1aed4abbd62a36
    Author: Raphael Bellec <raphael.bellec@gmail.com>
    
        Test with AoC demo input
    
    commit 5167274fdcda8d8cb2794535ef11514560077aa1
    Author: Raphael Bellec <raphael.bellec@gmail.com>
    
        Get all symbols
    
    commit ff66c0c5993ca57d4f4cd55e06eb923cc127637d
    Author: Raphael Bellec <raphael.bellec@gmail.com>
    
        Start to parse (slowly between work QA runs)
    
    commit 2aecd2e6cd2c8ac00d598fe95bacbf6f060501b4
    Author: Raphael Bellec <raphael.bellec@gmail.com>
    
        Create a 2d array with a map
    
    commit 502aeb17706d35fa0e8399a084adb5f173971541
    Author: Raphael Bellec <raphael.bellec@gmail.com>
    
        start to work on day 3 one day late.

---
+++ lib/day03.ex
@@ -0,0 +1,158 @@
+defmodule Day03.Match do
+  # Coord marks the position of the first letter of the match
+  defstruct [:type, :string, :coord, :int_value, :length, positions: []]
+end
+
+defmodule Day03.Part1 do
+  import Pathex
+  import Pathex.Lenses
+
+  def solve(input) do
+    {token_list, map_2d} = parse_input(input)
+
+    get_symbol_positions(token_list)
+    |> Enum.flat_map(&get_numbers_around_position(map_2d, &1))
+    |> Enum.sum()
+  end
+
+  # All position around a given position
+  def around({x, y}) do
+    [
+      {x - 1, y - 1},
+      {x, y - 1},
+      {x + 1, y - 1},
+      {x - 1, y},
+      {x + 1, y},
+      {x - 1, y + 1},
+      {x, y + 1},
+      {x + 1, y + 1}
+    ]
+  end
+
+  def get_numbers_around_position(map_2d, position) do
+    around(position)
+    |> Enum.map(&Map.get(map_2d, &1))
+    |> view(star() ~> matching(%{type: :number}))
+    |> case do
+      {:ok, numbers} -> Enum.uniq_by(numbers, & &1.coord) |> Enum.map(& &1.int_value)
+      :error -> []
+    end
+  end
+
+  def get_symbol_positions(token_list) do
+    token_list
+    |> view!(star() ~> matching(%{type: :symbol}) ~> path(:positions))
+    |> List.flatten()
+  end
+
+  def parse_input(input) do
+    tokens =
+      input
+      |> String.split("\n", trim: true)
+      |> Enum.with_index(fn line, line_index ->
+        parse_line(line, line_index)
+      end)
+      |> List.flatten()
+
+    map_2d =
+      tokens
+      |> Enum.flat_map(fn token ->
+        token.positions |> Enum.map(fn position -> {position, token} end)
+      end)
+      |> Enum.into(%{})
+
+    {tokens, map_2d}
+  end
+
+  # Note : symbols seems to be anything that is not a number or a dot
+  def parse_line(line, line_index) do
+    parse_type(line, line_index, ~r/\d+/, :number) ++
+      parse_type(line, line_index, ~r/[^\d\.]/, :symbol)
+  end
+
+  def parse_type(line, line_index, regexp, type) do
+    matches_str = Regex.scan(regexp, line)
+    positions = Regex.scan(regexp, line, return: :index)
+
+    Enum.zip(matches_str, positions)
+    |> Enum.map(fn {string_l, position_l} ->
+      # require IEx; IEx.pry
+      text = hd(string_l)
+      position = hd(position_l)
+
+      int_value =
+        case type do
+          :number -> String.to_integer(text)
+          _ -> nil
+        end
+
+      x = elem(position, 0)
+      length = elem(position, 1)
+      ends_at = x + length - 1
+
+      %Day03.Match{
+        type: type,
+        string: text,
+        coord: {x, line_index},
+        int_value: int_value,
+        length: length,
+        positions: Enum.map(x..ends_at, fn pos -> {pos, line_index} end)
+      }
+    end)
+  end
+end
+
+defmodule Day03.Part2 do
+  import Pathex
+  import Pathex.Lenses
+  import Day03.Part1
+
+  def solve(input) do
+    {token_list, map_2d} = parse_input(input)
+
+    gear_positions(token_list)
+    |> Enum.map(&handle_gear(&1, map_2d))
+    |> Enum.sum()
+  end
+
+  def handle_gear(position, map_2d) do
+    Day03.Part1.around(position)
+    |> Enum.map(&Map.get(map_2d, &1))
+    |> view(star() ~> matching(%{type: :number}))
+    |> case do
+      {:ok, numbers} ->
+        Enum.uniq_by(numbers, & &1.coord)
+        # Only consider gears with 2 numbers around
+        |> case do
+          [a, b] -> a.int_value * b.int_value
+          _ -> 0
+        end
+
+      :error ->
+        0
+    end
+  end
+
+  def gear_positions(token_list) do
+    token_list
+    |> view!(star() ~> matching(%{type: :symbol, string: "*"}) ~> path(:positions))
+    |> List.flatten()
+  end
+end
+
+defmodule Mix.Tasks.Day03 do
+  use Mix.Task
+
+  @spec run(any()) :: :ok
+  def run(_) do
+    input_filename = "inputs/day03.txt"
+    # input_filename = "inputs/demo-input.txt"
+    {:ok, input} = File.read(input_filename)
+
+    IO.puts("--- Part 1 ---")
+    IO.puts(Day03.Part1.solve(input))
+    IO.puts("")
+    IO.puts("--- Part 2 ---")
+    IO.puts(Day03.Part2.solve(input))
+  end
+end
---
+++ test/day03_test.exs
@@ -0,0 +1,148 @@
+defmodule DayO3Test do
+  import Day03.Part1
+  use ExUnit.Case
+
+  setup do
+    input_line = "467..+.114.."
+
+    demo_input_aoc = """
+    467..114..
+    ...*......
+    ..35..633.
+    ......#...
+    617*......
+    .....+.58.
+    ..592.....
+    ......755.
+    ...$.*....
+    .664.598..
+    """
+
+    # See https://www.reddit.com/r/adventofcode/comments/189q9wv/2023_day_3_another_sample_grid_to_use/
+    # symbols at
+    # [{9, 0}, {0, 1}, {7, 2}, {2, 4}, {11, 6}, {6, 7}, {1, 10}, {11, 10}, {8, 11}]
+    # values : Part 1: 925, Part 2: 6756
+    reddit_alternate_grid = """
+    12.......*..
+    +.........34
+    .......-12..
+    ..78........
+    ..*....60...
+    78.........9
+    .5.....23..$
+    8...90*12...
+    ............
+    2.2......12.
+    .*.........*
+    1.1..503+.56
+    """
+
+    %{
+      input_line: input_line,
+      demo_input_aoc: demo_input_aoc,
+      reddit_alternate_grid: reddit_alternate_grid
+    }
+  end
+
+  describe "Day 03 parsing" do
+    test "Parse a line", context do
+      expected = [
+        %Day03.Match{
+          type: :number,
+          string: "467",
+          coord: {0, 0},
+          int_value: 467,
+          length: 3,
+          positions: [{0, 0}, {1, 0}, {2, 0}]
+        },
+        %Day03.Match{
+          type: :number,
+          string: "114",
+          coord: {7, 0},
+          int_value: 114,
+          length: 3,
+          positions: [{7, 0}, {8, 0}, {9, 0}]
+        },
+        %Day03.Match{
+          type: :symbol,
+          string: "+",
+          coord: {5, 0},
+          int_value: nil,
+          length: 1,
+          positions: [{5, 0}]
+        }
+      ]
+
+      assert(expected == Day03.Part1.parse_line(context[:input_line], 0))
+    end
+  end
+
+  describe "Day 03 part 1" do
+    test "Find symbol positions", context do
+      {line_tokens, _} = Day03.Part1.parse_input(context[:input_line])
+      assert([{5, 0}] = Day03.Part1.get_symbol_positions(line_tokens))
+
+      assert(
+        [{3, 1}, {6, 3}, {3, 4}, {5, 5}, {3, 8}, {5, 8}] =
+          Day03.Part1.parse_input(context[:demo_input_aoc])
+          |> elem(0)
+          |> Day03.Part1.get_symbol_positions()
+      )
+
+      assert(
+        [{9, 0}, {0, 1}, {7, 2}, {2, 4}, {11, 6}, {6, 7}, {1, 10}, {11, 10}, {8, 11}] =
+          Day03.Part1.parse_input(context[:reddit_alternate_grid])
+          |> elem(0)
+          |> Day03.Part1.get_symbol_positions()
+      )
+    end
+
+    test "find around positions" do
+      assert(
+        [{0, 0}, {1, 0}, {2, 0}, {0, 1}, {2, 1}, {0, 2}, {1, 2}, {2, 2}] ==
+          Day03.Part1.around({1, 1})
+      )
+    end
+
+    test "get number arounds a position, simple case", context do
+      map_2d = parse_input(context[:reddit_alternate_grid]) |> elem(1)
+      assert([23, 90, 12] == get_numbers_around_position(map_2d, {6, 7}))
+    end
+
+    test "get number arounds a position, same numbers have multiple 'around positions'",
+         context do
+      map_2d = parse_input(context[:demo_input_aoc]) |> elem(1)
+      assert([467, 35] == get_numbers_around_position(map_2d, {3, 1}))
+    end
+
+    test "demo values", context do
+      assert(4361 == solve(context[:demo_input_aoc]))
+      assert(925 == solve(context[:reddit_alternate_grid]))
+    end
+  end
+
+  describe "Day 03 Part 2" do
+    test "find gear and other symbols positions", context do
+      tokens = parse_input(context[:reddit_alternate_grid]) |> elem(0)
+      symbols = Day03.Part1.get_symbol_positions(tokens)
+      gears = Day03.Part2.gear_positions(tokens)
+      assert([{9, 0}, {2, 4}, {6, 7}, {1, 10}, {11, 10}] == gears)
+      assert([{0, 1}, {7, 2}, {11, 6}, {8, 11}] == symbols -- gears)
+    end
+
+    test "handle gears", context do
+      {_, map_2d} = parse_input(context[:reddit_alternate_grid])
+
+      assert(0 == Day03.Part2.handle_gear({9, 0}, map_2d))
+      assert(78 * 78 == Day03.Part2.handle_gear({2, 4}, map_2d))
+      assert(0 == Day03.Part2.handle_gear({6, 7}, map_2d))
+      assert(0 == Day03.Part2.handle_gear({1, 10}, map_2d))
+      assert(12 * 56 == Day03.Part2.handle_gear({11, 10}, map_2d))
+    end
+
+    test "Solve part 2", context do
+      assert(467_835 == Day03.Part2.solve(context[:demo_input_aoc]))
+      assert(6756 == Day03.Part2.solve(context[:reddit_alternate_grid]))
+    end
+  end
+end
