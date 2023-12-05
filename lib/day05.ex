b5112b364c8802fa1dbcb6ed6dc0a4d2d4a8ee20
Raphaël B <raphael.bellec@gmail.com>
2023-12-06 00:15:07 +0100

    Day5 (#2)
    * Day5 parser first draft
    
    * parse ok
    
    * Add field to ranges
    
    * add "garden functions"
    
    * Works with demo data
    
    * Part 1

---
+++ lib/day05.ex
@@ -0,0 +1,122 @@
+# Define a function from a place to the next in the garden.
+defmodule Day05.GardenFunctionElement do
+
+  defstruct [:from, :to, :offset, :length, :source, :destination, :input_range, :image_range]
+
+  def convert_range(raw_range) do
+    length = raw_range |> Keyword.get(:length)
+    source = raw_range |> Keyword.get(:source)
+    destination = raw_range |> Keyword.get(:destination)
+    offset = destination - source
+
+    %Day05.GardenFunctionElement{
+      offset: offset,
+      length: length,
+      source: source,
+      destination: destination,
+      input_range: Range.new(source, source + length - 1),
+      image_range: Range.new(destination, destination + length - 1)
+    }
+  end
+
+  # Take an integer and apply the function.
+  # return nil if its
+  def apply_one(function_elem, location) do
+    case location in function_elem.input_range do
+      true -> location + function_elem.offset
+      false -> nil
+    end
+  end
+
+  def apply_garden_fun(list_of_elements, location) do
+    list_of_elements
+    |> Enum.map(&apply_one(&1, location))
+    # return current location if no movement was available
+    |> Enum.reject(&is_nil/1)
+    |> case do
+      [] -> [ location ]
+      new_locations -> new_locations
+    end
+  end
+
+  def apply_garden_fun_to_locations(list_of_elements, locations) do
+    # require IEx; IEx.pry
+    Enum.flat_map(locations, &apply_garden_fun(list_of_elements, &1))
+  end
+end
+
+defmodule Day05.Parser do
+  import NimbleParsec
+
+  spaces = times(string(" "), min: 1)
+  eol = choice([string("\r\n"), string("\n")])
+  sep = ignore(choice([spaces, eol]) |> times(min: 1))
+
+  colon = string(":")
+  number = ignore(spaces) |> integer(min: 1)
+
+  seeds = ignore(string("seeds:")) |> concat(times(number, min: 1) |> tag(:seeds))
+
+  # Garden places not needed in part 1 at least
+  misc_string = ascii_string([?a..?z, ?A..?Z, ?\-, ?\s], min: 1)
+  location_names = misc_string |> concat(colon)
+
+  range_definition =
+    integer(min: 1)
+    |> unwrap_and_tag(:destination)
+    |> concat(number |> unwrap_and_tag(:source))
+    |> concat(number |> unwrap_and_tag(:length))
+
+  garden_function =
+    ignore(location_names)
+    |> concat(
+      times(sep |> concat(range_definition) |> wrap, min: 1)
+      |> map({Day05.GardenFunctionElement, :convert_range, []})
+    )
+
+  file =
+    seeds
+    |> concat(times(sep |> concat(garden_function) |> wrap, min: 1) |> tag(:functions) )
+    # |> eos
+
+  defparsec(:seeds, seeds)
+  defparsec(:misc_string, misc_string)
+  defparsec(:location_names, location_names)
+  defparsec(:range_definition, range_definition)
+  defparsec(:garden_function, garden_function)
+  defparsec(:file, file)
+end
+
+defmodule Day05.Part1 do
+  # My first idea was to compose each non deterministic "function" and get a list of final functions representing the seed-to-location function
+  # But I did not find any working range arithmetic in elixir and I do not have time to write it.
+  def solve(input) do
+    data = Day05.Parser.file(input) |> elem(1)
+
+    seeds = Keyword.get(data, :seeds)
+    functions = Keyword.get(data, :functions)
+
+    final_locations(functions, seeds)
+    |> Enum.min()
+  end
+
+  def final_locations(functions, seeds) do
+    Enum.reduce(functions, seeds, &Day05.GardenFunctionElement.apply_garden_fun_to_locations/2)
+  end
+end
+
+defmodule Mix.Tasks.Day05 do
+  use Mix.Task
+
+  @spec run(any()) :: :ok
+  def run(_) do
+    input_filename = "inputs/day05.txt"
+    {:ok, input} = File.read(input_filename)
+
+    IO.puts("--- Part 1 ---")
+    IO.puts(Day05.Part1.solve(input))
+    IO.puts("")
+    # IO.puts("--- Part 2 ---")
+    # IO.puts(Day05.Part2.solve(input))
+  end
+end
---
+++ test/day05_test.exs
@@ -0,0 +1,199 @@
+defmodule DayO5Test do
+  use ExUnit.Case
+
+  setup do
+    seed_line = "seeds: 79 14 55 13"
+    location_line = "seed-to-soil map:"
+    range_line = "0 15 37"
+
+    garden_function_def = """
+    seed-to-soil map:
+    50 98 2
+    52 50 48
+    """
+
+    demo_input_aoc = """
+    seeds: 79 14 55 13
+
+    seed-to-soil map:
+    50 98 2
+    52 50 48
+
+    soil-to-fertilizer map:
+    0 15 37
+    37 52 2
+    39 0 15
+
+    fertilizer-to-water map:
+    49 53 8
+    0 11 42
+    42 0 7
+    57 7 4
+
+    water-to-light map:
+    88 18 7
+    18 25 70
+
+    light-to-temperature map:
+    45 77 23
+    81 45 19
+    68 64 13
+
+    temperature-to-humidity map:
+    0 69 1
+    1 0 69
+
+    humidity-to-location map:
+    60 56 37
+    56 93 4
+    """
+
+    demo_functions = [
+      %Day05.GardenFunctionElement{
+        from: nil,
+        to: nil,
+        offset: -48,
+        length: 2,
+        source: 98,
+        destination: 50,
+        image_range: 50..51,
+        input_range: 98..99
+      },
+      %Day05.GardenFunctionElement{
+        from: nil,
+        to: nil,
+        offset: 2,
+        length: 48,
+        source: 50,
+        destination: 52,
+        image_range: 52..99,
+        input_range: 50..97
+      }
+    ]
+
+    demo_functions_2 = [
+      %Day05.GardenFunctionElement{
+        from: nil,
+        to: nil,
+        offset: -15,
+        length: 37,
+        source: 15,
+        destination: 0,
+        input_range: 15..51,
+        image_range: 0..36
+      },
+      %Day05.GardenFunctionElement{
+        from: nil,
+        to: nil,
+        offset: -15,
+        length: 2,
+        source: 52,
+        destination: 37,
+        input_range: 52..53,
+        image_range: 37..38
+      },
+      %Day05.GardenFunctionElement{
+        from: nil,
+        to: nil,
+        offset: 39,
+        length: 15,
+        source: 0,
+        destination: 39,
+        input_range: 0..14,
+        image_range: 39..53
+      }
+    ]
+
+    %{
+      seed_line: seed_line,
+      location_line: location_line,
+      range_line: range_line,
+      garden_function_def: garden_function_def,
+      demo_input_aoc: demo_input_aoc,
+      demo_functions: demo_functions,
+      demo_functions_2: demo_functions_2
+    }
+  end
+
+  describe "Day 05 parsing" do
+    import Day05.Parser
+
+    test "Parse cases", context do
+      seed = seeds(context.seed_line) |> elem(1)
+      location = location_names(context.location_line) |> elem(1)
+      loc_string = misc_string("seed-to-soil map:")
+      range = range_definition(context.range_line) |> elem(1)
+      # require IEx; IEx.pry
+
+      # Intermediate parser
+      assert(:ok = loc_string |> elem(0))
+      # ":" remain to parse after.
+      assert(":" = loc_string |> elem(2))
+
+      assert([seeds: [79, 14, 55, 13]] = seed)
+      assert(["seed-to-soil map", ":"] = location)
+      assert([destination: 0, source: 15, length: 37] = range)
+    end
+
+    test "parse a between location function", context do
+      parsed_result = garden_function(context.garden_function_def) |> elem(1)
+
+      assert(context.demo_functions == parsed_result)
+    end
+
+    test "parse file", _context do
+      # parsed_result = file(context.demo_input_aoc) |> elem(1)
+      # require IEx; IEx.pry
+      # Parse is ok and no time to unit test it.
+    end
+
+  end
+
+  # describe "Compose offset on range functions" do
+  #   # Range 1    |-------------|
+  #   # on Range 2       |-------------|
+  #   #
+  #   test "simple application on two intersecting ranges", _context do
+
+  #   end
+  # end
+
+  describe "applying garden functions" do
+    import Day05.GardenFunctionElement
+    test "apply one", context do
+      # source: 98, destination: 50, offset: -48, length: 2
+      # require IEx; IEx.pry
+      assert( apply_one(Enum.at(context.demo_functions, 0),  97) == nil )
+      assert( apply_one(Enum.at(context.demo_functions, 0),  98) == 50 )
+      assert( apply_one(Enum.at(context.demo_functions, 0),  99) == 51 )
+      assert( apply_one(Enum.at(context.demo_functions, 0), 100) == nil )
+
+      assert( apply_one(Enum.at(context.demo_functions, 1),  49) == nil )
+      assert( apply_one(Enum.at(context.demo_functions, 1),  50) == 52 )
+    end
+
+    test "apply_garden_fun", context do
+      # require IEx; IEx.pry
+      assert( apply_garden_fun(context.demo_functions, 98) == [50] )
+      assert( apply_garden_fun(context.demo_functions, 50) == [52] )
+      assert( apply_garden_fun(context.demo_functions, 10) == [10] )
+
+      assert( apply_garden_fun(context.demo_functions_2, 81) == [81] )
+    end
+
+    test "apply_garden_fun to a list of seed", context do
+      # require IEx; IEx.pry
+      test = fn data, expected ->
+        apply_garden_fun_to_locations(context.demo_functions, data) == expected
+      end
+      assert(test.([1, 2, 3], [1, 2, 3]))
+      assert(test.([1, 98], [1, 50]))
+      assert(test.([1, 98, 50], [1, 50, 52]))
+      assert(test.([79, 14, 55, 13], [81, 14, 57, 13]))
+    end
+  end
+
+  test "solve", context do
+    assert(Day05.Part1.solve(context.demo_input_aoc) == 35)
+  end
+end
